4137682c8eddd01095bbcd609493e15baa3a901e
Rajat Rasal <yugiohrajat1@gmail.com>
2022-12-23 02:21:36 +0000

    Training with 1 sentence

--- src/vae.py
+++ src/vae.py
@@ -2,7 +2,9 @@ from typing import List
 
 import pytorch_lightning as pl
 import torch
+import torch.nn.functional as F
 from torch import nn
+from torch.utils.data import DataLoader, Dataset
 from transformers import (  # noqa: E501
     BertModel,
     BertTokenizer,
@@ -18,11 +20,11 @@ class VAE(pl.LightningModule):
 
         # Encoder Tokeniser
         self.tokeniser_encoder = BertTokenizer.from_pretrained(
-            "bert-base-cased"
+            "bert-base-uncased"
         )
 
         # Encoder Model
-        self.model_encoder = BertModel.from_pretrained("bert-base-cased")
+        self.model_encoder = BertModel.from_pretrained("bert-base-uncased")
 
         # Decoder Tokeniser
         self.tokeniser_decoder = GPT2Tokenizer.from_pretrained("gpt2")
@@ -55,7 +57,7 @@ class VAE(pl.LightningModule):
         eps = torch.randn_like(std)
         return eps.mul(std).add_(mean)
 
-    def forward(self, sentences: List[str]):
+    def _step(self, sentences: List[str]):  # , batch_idx):
         # Tokenise for encoder and decoder
         enc_tokens = self.tokeniser_encoder(
             sentences,
@@ -85,7 +87,7 @@ class VAE(pl.LightningModule):
         )
 
         # [batch_size, num_heads, seq_length = 1, head_dim]
-        m = [_latent.view(3, 12, 1, 64) for _latent in memory_latent_per_layer]
+        m = [_l.view(-1, 12, 1, 64) for _l in memory_latent_per_layer]
         m = tuple(zip(m, m))
 
         # Decoding
@@ -98,14 +100,62 @@ class VAE(pl.LightningModule):
             labels=dec_tokens["input_ids"],
             return_dict=True,
         )
-        print(outputs["loss"])
+        return outputs
+        # return outputs["loss"]
 
     def training_step(self, batch, batch_idx):
-        self.forward()
-        pass
+        return self._step(batch)["loss"]
+
+    def validation_step(self, batch, batch_idx):
+        logits = self._step(batch).logits
+        softmax = F.softmax(logits)
+        argmax = torch.argmax(softmax, dim=2)
+        recons = self.tokeniser_decoder.batch_decode(
+            argmax.tolist(), skip_special_tokens=True
+        )
+        for orig, recon in zip(batch, recons):
+            print()
+            print(orig)
+            print("".join(recon))
+            print()
+
+    def configure_optimizers(self):
+        return torch.optim.AdamW(self.parameters(), lr=5e-2)
+
+
+class MyDataset(Dataset):
+    def __init__(self, data):  # , targets, transform=None):
+        self.data = data
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return len(self.data)
 
 
 if __name__ == "__main__":
     model = VAE()
-    sentences = ["xxx", "yyy", "my name is rajat"]
-    model(sentences)
+
+    # lines = []
+    # with open("./data/wikipedia.segmented.nltk.txt", "r") as f:
+    #     for i in range(100):
+    #         line = f.readline()
+    #         tokens = model.tokeniser_encoder.encode(line)
+    #         if len(tokens) <= 64:
+    #             print(i, line)
+    #             lines.append(line[:-1])
+
+    lines = [
+        "the little girl plays with the toys",
+        "the children are watching a show.",
+    ]
+    dataset = MyDataset(lines)
+    train_dataloader = DataLoader(
+        dataset, batch_size=5, collate_fn=lambda xs: xs
+    )
+    val_dataloader = DataLoader(  # noqa: E501
+        dataset, batch_size=5, collate_fn=lambda xs: xs
+    )
+    trainer = pl.Trainer(max_epochs=40, check_val_every_n_epoch=1)
+    trainer.fit(model, train_dataloader, val_dataloader)
